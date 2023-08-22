7957da76e612a18c84bb8762a6eacd0024387e2d
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-08-22 10:37:46 +0530

    links added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Projects.module.css"; // Import the CSS module
 import dashboard from "./images/dashboard-templates.jpg";
-import growth from "./images/growth.jpg"
-import gitapi from "./images/githubapi.png"
+import growth from "./images/growth.jpg";
+import gitapi from "./images/githubapi.png";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Projects = () => {
@@ -16,10 +16,20 @@ const Projects = () => {
           <img style={{ width: "25rem" }} src={dashboard} alt="Project Image" />
         </div>
         <div className={styles["project-details"]}>
-          <h2>Zero Dashboard</h2>
+          <h2>
+            {" "}
+            <a
+              href="https://github.com/reerajput930/zero-dashboard"
+              target="_blank"
+            >
+              Zero Dashboard{" "}
+            </a>
+          </h2>
           <p>
-          A dashboard is an information management tool that receives data from a linked database to provide data visualizations. It typically offers high-level information in one view that end users can use to answer a single question.
-          Used MERN Tech Stack.
+            A dashboard is an information management tool that receives data
+            from a linked database to provide data visualizations. It typically
+            offers high-level information in one view that end users can use to
+            answer a single question. Used MERN Tech Stack.
           </p>
           <div className={styles["project-skills"]}>
             <ul>
@@ -31,19 +41,29 @@ const Projects = () => {
           </div>
           <a
             href="https://github.com/reerajput930/zero-dashboard"
-            className={styles["github-link"]}
             target="_blank"
+            className={styles["github-link"]}
           >
             <FontAwesomeIcon className={styles.github} icon={faGithub} />
           </a>
         </div>
       </div>
       <div id={styles.middle} className={styles["project-card"]}>
-        <div className={`${styles["project-details-left"]} ${styles["project-details"]}`}>
-          <h2>Github Follawers API</h2>
+        <div
+          className={`${styles["project-details-left"]} ${styles["project-details"]}`}
+        >
+          <h2>
+            {" "}
+            <a
+              href="https://github.com/reerajput930/Github-followers-api"
+              target="_blank"
+            >
+              Github Follawers API
+            </a>
+          </h2>
           <p>
-          Provide all the github followers on the page .Used Reactjs and Github
-          followers Api , have Pagination and Filteration feature.
+            Provide all the github followers on the page .Used Reactjs and
+            Github followers Api , have Pagination and Filteration feature.
           </p>
           <div className={styles["project-skills"]}>
             <ul>
@@ -55,8 +75,8 @@ const Projects = () => {
           </div>
           <a
             href="https://github.com/reerajput930/Github-followers-api"
-            className={styles["github-link"]}
             target="_blank"
+            className={styles["github-link"]}
           >
             <FontAwesomeIcon className={styles.github} icon={faGithub} />
           </a>
@@ -70,10 +90,18 @@ const Projects = () => {
           <img style={{ width: "25rem" }} src={growth} alt="Project Image" />
         </div>
         <div className={styles["project-details"]}>
-          <h2>Growth Application</h2>
+          <h2>
+            {" "}
+            <a
+              href="https://github.com/reerajput930/growth-app"
+              target="_blank"
+            >
+              Growth Application{" "}
+            </a>
+          </h2>
           <p>
-          Growth Application is a Todo app , which is build for two users
-          only.This project have the jira software of drag and drop feature.
+            Growth Application is a Todo app , which is build for two users
+            only.This project have the jira software of drag and drop feature.
           </p>
           <div className={styles["project-skills"]}>
             <ul>
@@ -85,14 +113,13 @@ const Projects = () => {
           </div>
           <a
             href="https://github.com/reerajput930/growth-app"
-            className={styles["github-link"]}
             target="_blank"
+            className={styles["github-link"]}
           >
             <FontAwesomeIcon className={styles.github} icon={faGithub} />
           </a>
         </div>
       </div>
-
     </div>
   );
 };
