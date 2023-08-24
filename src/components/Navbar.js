03a9fc5a35ed12018a2363b2b8181608e8e9eb5b
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-08-25 00:43:23 +0530

    navbar issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -34,16 +34,16 @@ export default function Navbar() {
                       <li>HOME</li>
                     </Link> */}
                 <li>
-                  <a href="#about"> <h2><span className={style.colored}>01.</span> About</h2></a>
+                  <a onClick={()=> handleMenuToggle()} href="#about"> <h2><span className={style.colored}>01.</span> About</h2></a>
                 </li>
                 <li>
-                  <a href="#skills--section"> <h2><span className={style.colored}>02.</span>Skills</h2></a>
+                  <a onClick={()=> handleMenuToggle()} href="#skills--section"> <h2><span className={style.colored}>02.</span>Skills</h2></a>
                 </li>
                 <li>
-                  <a href="#experence"> <h2><span className={style.colored}>03.</span>Experience</h2></a>
+                  <a onClick={()=> handleMenuToggle()} href="#experence"> <h2><span className={style.colored}>03.</span>Experience</h2></a>
                 </li>
                 <li>
-                  <a href="#projects"> <h2><span className={style.colored}>04.</span>Projects</h2></a>
+                  <a onClick={()=> handleMenuToggle()} href="#projects"> <h2><span className={style.colored}>04.</span>Projects</h2></a>
                 </li>
                 
               </ul>
