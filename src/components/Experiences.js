d656411389b2698cb511276e3d3aff52c9e25869
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-08-25 23:34:40 +0530

    usefull updation

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,5 +1,6 @@
 import "../index.css";
 import style from "./Experiences.module.css";
+import arrow from "./images/arrow-turn-down-right-svgrepo-com.png";
 
 export default function Experiences() {
   return (
@@ -17,19 +18,60 @@ export default function Experiences() {
           <div className="timeline-content">
             <h4>ReactJs Developer · Delhi</h4>
             <p>
-              {" "}
-              Embarking on a exciting{" "}
-              <span className={style.highlight}>
-                three-month paid internship
-              </span>{" "}
-              at Reverr, a{" "}
-              <span className={style.highlight}>Product Based StartUp</span> at
-              Delhi,India.
               <span className={style.highlight}>
                 {" "}
                 Leading the frontend team{" "}
               </span>{" "}
-              while crafting in React.js for an impactful experience.
+              at Reverr{" "}
+              <span className={style.highlight}>Product Based StartUp</span> at
+              Delhi,India.
+            </p>
+            <p className={style.lowerPara}>
+              <span>
+                <img src={arrow} />
+                Till now solved more than 85+ assigned tasks on jira and trello
+              </span>
+              <span>
+                <img src={arrow} />
+                Designed the{" "}
+                <a target="_blank" href="https://reverr.io/" className="link">
+                  {" "}
+                  Reverr Website{" "}
+                </a>
+              </span>
+              <span>
+                <img src={arrow} />
+                Designed and build the logic of{" "}
+                <a target="_blank" href="https://webapp.reverr.io/" className="link">
+                  {" "}
+                  Reverr Community web-app
+                </a>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  space Filter (logic and ui)
+                </span>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  Upload Section (logic and ui)
+                </span>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  Posts Section (logic and ui)
+                </span>
+              </span>
+
+              <span className={style.techskills}>
+                <ul>
+                  <li> #ReactJs</li>
+                  <li> #FireBase</li>
+                  <li>#AWSSerwe</li>
+                  <li>#Redux</li>
+                  <li>#RestApi</li>
+                </ul>
+              </span>
             </p>
           </div>
         </div>
@@ -41,39 +83,53 @@ export default function Experiences() {
           <div className="timeline-content">
             <h4>Junior Software Engineer · Bangalore</h4>
             <p>
-              {" "}
-              Spiced up my career with a sizzling{" "}
-              <span className={style.highlight}>
-                {" "}
-                three-month internship
-              </span>{" "}
-              as a
-              <span className={style.highlight}>
-                Junior Software Engineer{" "}
-              </span>{" "}
-              at Eduladder Company, igniting innovation from{" "}
-              <span className={style.highlight}>Bangalore, India </span> .
+              <span className={style.highlight}>Junior Software Engineer </span>{" "}
+              at Eduladder Company, which is{" "}
+              <span className={style.highlight}>e-learning platform </span>{" "}
+              platform at Bangalore, India
+            </p>
+
+            <p className={style.lowerPara}>
+              <span>
+                <img src={arrow} />
+                Linux Command lines
+              </span>
+              <span>
+                <img src={arrow} />
+                Solved more than 75+ bugs on the e-commerse platform
+              </span>
+
+              <span className={style.techskills}>
+                <ul>
+                  <li> #ReactJs</li>
+                  <li>#JS</li>
+                  <li> #html5</li>
+                  <li>#css3</li>
+                  <li>#linux</li>
+                </ul>
+              </span>
             </p>
           </div>
         </div>
 
         <div className="timeline-row">
           <div className="timeline-time">
-            Feb 2022 - feb 2022<small>Part-Time</small>
+           <small>Part-Time</small>
           </div>
           <div className="timeline-content">
             <h4>Freelancing · Dubai · India </h4>
             <p>
-              Crafted captivating{" "}
-              <span className={style.highlight}> WordPress websites </span>{" "}
-              through <span className={style.highlight}> freelancing </span>,
-              including the creation of{" "}
-              <a target="_blank" href="https://divulgejute.com">
+              Created pure (html5 , css3 ) Dubai Website - <a className="link" target="_blank" href="https://shehban-services-est.netlify.app/">SHEHBAN TECHNICAL SERVICES EST</a>
+             
+              {/* <a target="_blank" href="https://divulgejute.com">
                 <span className={style.highlight}>
                   {" "}
                   https://divulgejute.com/{" "}
                 </span>
-              </a>
+              </a> */}
+            </p>
+            <p>
+              Indian e-commerce website - <a className="link" target="_blank" href="https://divulgejute.com/">Divulge Jute</a>
             </p>
           </div>
         </div>
@@ -85,11 +141,59 @@ export default function Experiences() {
           <div className="timeline-content">
             <h4>Web Developer · Germany</h4>
             <p>
-              In three thrilling months, I harnessed my{" "}
-              <span className={style.highlight}> web development </span> prowess
-              during a paid internship at the cutting-edge{" "}
-              <span className={style.highlight}> Germany-based startup </span>,
+              In three thrilling months, I had worked upon the   <span className={style.highlight}> Logic Builing </span> in
+              Javascript at
+              <span className={style.highlight}> Germany-based consultant startup </span>,
               How To Abroad.
+            </p>
+
+            <p className={style.lowerPara}>
+              <span>
+                <img src={arrow} />I have designed and built the entire{" "}
+                <span className={style.highlight}>
+                  {" "}
+                  college filtering system{" "}
+                </span>
+                for{" "}
+                <a
+                  href="https://howtoabroad.com/find-study-programmes-in-germany/"
+                  className="link"
+                >
+                  {" "}
+                  HowToAbroad in Germany
+                </a>{" "}
+                , The added functionalities include the following:
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  Pagination feature
+                </span>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  University Name ,Course Name and Germany Ranking (text filter)
+                </span>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  Duration and sortby (menu filter)
+                </span>
+                <span>
+                  {" "}
+                  <img src={arrow} />
+                  Tution Fee (Range Bar Filter)
+                </span>
+              </span>
+
+              <span className={style.techskills}>
+                <ul>
+                  <li> #Javascript</li>
+                  <li> #webScraping</li>
+                  <li>#html5</li>
+                  <li>#css3</li>
+                  <li> #WordPress</li>
+                </ul>
+              </span>
             </p>
           </div>
         </div>
@@ -101,8 +205,17 @@ export default function Experiences() {
           <div className="timeline-content">
             <h4>Google Developer Student Clubs</h4>
             <p>
-              Empowering as Co-Lead of Competitive Programming, I've taught over  <span className={style.highlight}>  300+ students  </span>in coding languages like  <span className={style.highlight}>  HTML, CSS, JS, along with  data structures </span>, through   <span className={style.highlight}>  20+ engaging
-              seminars </span>, while championing   <span className={style.highlight}> gender diversity</span>in tech.
+              Empowering as Co-Lead of Competitive Programming, I've taught over{" "}
+              <span className={style.highlight}> 300+ students </span>in coding
+              languages like{" "}
+              <span className={style.highlight}>
+                {" "}
+                HTML, CSS, JS, along with data structures{" "}
+              </span>
+              , through{" "}
+              <span className={style.highlight}> 20+ engaging seminars </span>,
+              while championing{" "}
+              <span className={style.highlight}> gender diversity</span>in tech.
             </p>
           </div>
         </div>
@@ -112,7 +225,17 @@ export default function Experiences() {
           </div>
           <div className="timeline-content">
             <h4>Microsoft Learn Student Ambassador</h4>
-            <p>Engaged in more than <span className={style.highlight}> 150+ global tech events </span>, and  <span className={style.highlight}> Conducted in over  45+ </span>, and had the honor of being <span className={style.highlight}>interviewed by a Microsoft employee </span> .</p>
+            <p>
+              Engaged in more than{" "}
+              <span className={style.highlight}> 150+ global tech events </span>
+              , and{" "}
+              <span className={style.highlight}> Conducted in over 45+ </span>,
+              and had the honor of being{" "}
+              <span className={style.highlight}>
+                interviewed by a Microsoft employee{" "}
+              </span>{" "}
+              .
+            </p>
           </div>
         </div>
 
@@ -122,7 +245,14 @@ export default function Experiences() {
           </div>
           <div className="timeline-content">
             <h4>GirlScript Summer of Code</h4>
-            <p>Learned <span className={style.highlight}> Open-Source contribution </span> and <span className={style.highlight}> Team work </span>.</p>
+            <p>
+              Learned{" "}
+              <span className={style.highlight}>
+                {" "}
+                Open-Source contribution{" "}
+              </span>{" "}
+              and <span className={style.highlight}> Team work </span>.
+            </p>
           </div>
         </div>
       </div>
